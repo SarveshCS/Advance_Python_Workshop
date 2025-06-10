764c6f391994f3e056658ca2a9a2ad704d1d9a2b
Sarvesh Mishra <sarveshmishra.py@gmail.com>
2025-06-10 11:00:53 +0530

    added: data_types.md

---
+++ Day_1/data_types.md
@@ -0,0 +1,12 @@
+# Data Types in Python
+
+## Numbers:-
+
+- integer -> `0 to 9`
+- float   -> `ie. 4.56`
+- complex -> `ie. 5i + 4'
+
+## Boolean:-
+
+- True
+- False
---
+++ Day_1/data_types_in_python.py
@@ -0,0 +1,22 @@
+"""
+primitive:
+
+int
+char            
+float           
+double          
+
+
+---------
+string
+tuple
+set
+dict
+list
+boolean
+numbers
+
+"""
+
+
+print(5 and 7)
